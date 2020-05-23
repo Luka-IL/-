74dc2658f50f35d030a930f9faebe6e7f7baa296
MoGo <dert_94@users.noreply.github.com>
2020-05-23 12:30:40 +0300

    Добавил анимацию input в header

--- js/js.js
+++ js/js.js
@@ -1,5 +1,19 @@
-$(document).ready(function(){
-    $('.good').on('click', function(e){
-                this.style.backgroundColor.black;
-                })
+$(document).ready(function () {
+
+    $('.mom-item-circle').hover(function() {
+        $(this).addClass('circle-hover');
+        $(this).prev().addClass('label-hover');
+    }, function() {
+        $(this).removeClass('circle-hover');
+        $(this).prev().removeClass('label-hover');
+    })
+    
+   $('.mom-item-label').hover(function() {
+        $(this).addClass('label-hover');
+        $(this).next().addClass('circle-hover');
+    }, function() {
+        $(this).removeClass('label-hover');
+        $(this).next().removeClass('circle-hover');
+    }) 
+    
 })
